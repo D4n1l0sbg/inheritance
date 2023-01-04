b830eb65b9e907d974b464cd35fda6249104c48c
Danilo Souza <danilosbg50@gmail.com>
2023-01-03 23:22:12 -0300

    Alpha v1.1

--- src/application/program.java
+++ src/application/program.java
@@ -1,15 +1,16 @@
 package application;
 
+import entites.ImportedProduct;
 import entites.Product;
 
-import java.util.ArrayList;
-import java.util.List;
-import java.util.Locale;
-import java.util.Scanner;
+import java.text.ParseException;
+import java.text.SimpleDateFormat;
+import java.util.*;
 
 public class program {
-    public static void main(String[] args) {
+    public static void main(String[] args) throws ParseException {
         Locale.setDefault(Locale.US);
+        SimpleDateFormat sdf = new SimpleDateFormat("dd/MM/yyyy");
         Scanner sc = new Scanner(System.in);
 
         List<Product>list = new ArrayList<>();
@@ -21,23 +22,19 @@ public class program {
            System.out.println("Product #" + i + " data: ");
            System.out.print("Common, used or imported (c/u/i)? ");
            char type = sc.nextLine().charAt(0);
+            System.out.print("Name: ");
+            String name = sc.nextLine();
+            Double price = sc.nextDouble();
            if(type == 'c' ){
-               System.out.print("Name: ");
-               String name = sc.nextLine();
-               Double price = sc.nextDouble();
                list.add(new Product(name, price));
-
            }else if(type == 'u') {
-               System.out.print("Name: ");
-               String name = sc.nextLine();
-               System.out.print("Price");
-               Double price = sc.nextDouble();
+               System.out.print("Manufacture date (dd/MM/yyyy: ");
+               Date date = sdf.parse(sc.next());
                list.add(new Product());
            }else if(type == 'i'){
-               System.out.print("Name: ");
-               String name = sc.nextLine();
-               System.out.print("Price: ");
-               Double price = sc.nextDouble();
+               System.out.print("Customs Fee: ");
+               Double customsFee = sc.nextDouble();
+               list.add(new ImportedProduct(name, price, customsFee));
            }
         }
         sc.close();
